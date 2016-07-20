3d12a27816b9934c0001ec39ce575150bfec6cee
thiyansen <thiyansen@gmail.com>
2016-07-20 15:49:10 +0530

    Create Reverse.py

---
+++ Reverse.py
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Palindrome {
+private static Scanner in;
+
+	public static void main(String[] args) {
+		String input1= new String();
+		in = new Scanner(System.in);
+		input1 = in.next();
+		boolean output1;
+		StringBuffer s = new StringBuffer(input1);
+		s.reverse();
+        input1 = new String(s); 
+		System.out.println(input1);		
+	}
+}
+		
+	
